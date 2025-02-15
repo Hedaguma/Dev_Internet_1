fd0f8e891b78a56b7fc31e0cb577e3ec2a9b5780
Hector Daniel Guzman Magaña <vector981@hotmail.com>
2025-02-15 14:19:12 -0600

    Eliminar directorio

--- Dev_Internet_1/rsc/menu.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    fetch('Menu.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('menu-contenedor').innerHTML = data;
-    });
-};
